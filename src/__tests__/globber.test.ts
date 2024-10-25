06bb8509db93e5e99d019ea7896c8cf6c01f1afa
Eli <88557639+lishaduck@users.noreply.github.com>
2024-10-25 09:12:11 -0500

    fix: manifests

--- src/__tests__/globber.test.ts
+++ src/__tests__/globber.test.ts
@@ -1,8 +1,8 @@
-import { join } from 'node:path'
+import * as path from 'node:path'
 import { createGlobber } from '../globber'
 
 test('returns the current directory as a match', async () => {
   const result = await createGlobber().glob(process.cwd(), 'package.json')
   expect(result).toHaveLength(1)
-  expect(result[0]).toBe(join(process.cwd(), 'package.json'))
+  expect(result[0]).toBe(path.join(process.cwd(), 'package.json'))
 })
