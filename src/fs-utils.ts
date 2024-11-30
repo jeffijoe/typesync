404de285d0e0ff95c1f21ef2d0f6ba0cc3641a78
Eli <88557639+lishaduck@users.noreply.github.com>
2024-11-30 10:19:10 -0600

    fix: race condition
    This is explicitly warned against in the Node documentation,
    to the point that Deno doesn't even support doing this (easily).
    This is also a potential bug Windows bug as well.
    And best part of all: the new code is shorter!

--- src/fs-utils.ts
+++ src/fs-utils.ts
@@ -1,25 +1,13 @@
-import { readFile, stat } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 
 export async function readFileContents(filePath: string): Promise<string> {
-  await assertFile(filePath)
-  return readFile(filePath, 'utf-8')
-}
-
-async function assertFile(filePath: string) {
-  if (!(await existsAsync(filePath))) {
-    throw new Error(`${filePath} does not exist.`)
-  }
-}
-
-async function existsAsync(filePath: string): Promise<boolean> {
   try {
-    await stat(filePath)
-    return true
+    return readFile(filePath, 'utf-8')
   } catch (err) {
-    /* v8 ignore next 5 */
-    if ((err as { code: string }).code === 'ENOENT') {
-      return false
+    if ((err as NodeJS.ErrnoException).code === 'ENOENT') {
+      throw new Error(`${filePath} does not exist.`)
     }
+
     throw err
   }
 }
